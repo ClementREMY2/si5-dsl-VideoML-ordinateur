ec721a4a00115eca3919a8e870d0ad2c578e0a54
Alexis Malosse <alexis.malosse@etu.unice.fr>
2024-12-11 14:41:49 +0100

    feat: check video path

--- VideoML/generated/First_Example.py
+++ VideoML/generated/First_Example.py
@@ -1,10 +1,10 @@
 import moviepy
 
 # Load the video clip
-video_video1 = moviepy.VideoFileClip("path/to/fiel")
+video_video1 = moviepy.VideoFileClip("/Users/alexismalosse/NotInCloud/Courses/S9/B2/DSL/si5-dsl-VideoML-ordinateur/demo/first_sample.mp4")
 
 # Load the video clip
-video_video2 = moviepy.VideoFileClip("path/to/fiel")
+video_video2 = moviepy.VideoFileClip("/Users/alexismalosse/NotInCloud/Courses/S9/B2/DSL/si5-dsl-VideoML-ordinateur/demo/second_sample.mp4")
 
 # Export the final video
 final_video.write_videofile("output.mp4")
--- VideoML/src/language-server/video-ml-validator.ts
+++ VideoML/src/language-server/video-ml-validator.ts
@@ -2,8 +2,11 @@ import { ValidationAcceptor, ValidationChecks } from 'langium';
 import {
     VideoProject,
     VideoMlAstType,
+    Video,
 } from './generated/ast';
 import type { VideoMlServices } from './video-ml-module';
+import fs from 'fs';
+import path from 'path';
 
 /**
  * Register custom validation checks.
@@ -12,8 +15,8 @@ export function registerValidationChecks(services: VideoMlServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.VideoMlValidator;
     const checks: ValidationChecks<VideoMlAstType> = {
-        // Check if video output name is a valid file name
         VideoProject: validator.checkVideoProject,
+        Video: validator.checkVideo,
     };
     registry.register(checks, validator);
 }
@@ -23,8 +26,11 @@ export function registerValidationChecks(services: VideoMlServices) {
  */
 export class VideoMlValidator {
     checkVideoProject(videoProject: VideoProject, accept: ValidationAcceptor): void {
-        // Check if video output name is a valid file name
         this.checkOutputFileName(videoProject, accept);
+    }
+
+    checkVideo(video: Video, accept: ValidationAcceptor): void {
+        this.checkVideoPath(video, accept);
     }
 
     // Check if video output name is a valid file name
@@ -34,6 +40,19 @@ export class VideoMlValidator {
             if (!regex.test(videoProject.outputName)) {
                 accept('error', 'Invalid output file name', { node: videoProject, property: 'outputName' });
             }
+        }
+    }
+
+    checkVideoPath(video: Video, accept: ValidationAcceptor): void {
+        // TODO : Make program usable without absolute path
+        // Check if path is an absolute path
+        if (!path.isAbsolute(video.path)) {
+            accept('error', 'Video path must be an absolute path', { node: video, property: 'path' });
+        }
+
+        // Check if file exists
+        if (!fs.existsSync(video.path)) {
+            accept('error', 'Video file not found', { node: video, property: 'path' });
         }
     }
 }
