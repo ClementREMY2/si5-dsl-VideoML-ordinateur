29841acd6d1362dfa594b9cd3d481796dddb520e
Alexis Malosse <alexis.malosse@etu.unice.fr>
2025-01-08 09:52:11 +0100

    feat(ui): open dev tools on open #25

--- VideoML/ui/electron/main.ts
+++ VideoML/ui/electron/main.ts
@@ -38,6 +38,9 @@ function createWindow() {
     },
   })
 
+  // Open the DevTools.
+  // win.webContents.openDevTools();
+
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', (new Date).toLocaleString())
